d8d0c5493ac45644a059ef62a748abf87578ede5
TechCare <huy48552@donga.edu.vn>
2022-03-15 18:32:44 +0700

    initial

---
+++ .history/src/components/Prize_20220309215722.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './CSS/Prize.css';
+import { AiOutlineStar } from 'react-icons/ai';
+import icon1 from '../image/about-2.jpg';
+import icon3 from '../image/about-2.jpg';
+import icon4 from '../image/about-2.jpg';
+
+const Prize = () => {
+    return (
+        <div className='prize'>
+            <article className='prize-wrap'>
+                <section className='prize-item'>
+                    <img src={icon1} alt='prize-img'></img>
+                    <p>Open Table Dinners</p>
+                    <div>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                    </div>
+                </section>
+                <section className='prize-item'>
+                    <img src={icon2} alt='prize-img'></img>
+                    <p>Open Table Dinners</p>
+                    <div>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                    </div>
+                </section>
+                <section className='prize-item'>
+                    <img src={icon3} alt='prize-img'></img>
+                    <p>Open Table Dinners</p>
+                    <div>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                    </div>
+                </section>
+                <section className='prize-item'>
+                    <img src={icon4}></img>
+                    <p>Open Table Dinners</p>
+                    <div>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                        <span>
+                            <AiOutlineStar className='prize-item-star' />
+                        </span>
+                    </div>
+                </section>
+            </article>
+        </div>
+    );
+};
+
+export default Prize;
---
+++ .history/src/pages/Contact_20220309222510.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import './CSS/Contact.css';
+
+const Contact = () => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+    const [valid, setValid] = useState({
+        nameValid: false,
+        emailValid: false,
+        messageValid: false,
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setValid(false);
+        setValues({
+            ...values,
+            [name]: value,
+        });
+    };
+
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+        const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if (!values.message.trim()) {
+            setValid({ ...valid, messageValid: true });
+        } else {
+            if (values.name && regexEmail.test(values.email)) {
+                alert('Success');
+                console.log(values);
+            }
+        }
+
+        if (!regexEmail.test(values.email)) {
+            setValid({ ...valid, emailValid: true });
+        }
+        if (!values.name.trim()) {
+            setValid({ ...valid, nameValid: true });
+        }
+    };
+
+    return (
+        <div className='contact'>
+            <div className='contact-img'>
+                <h1>Contact</h1>
+            </div>
+            <div className='container'>
+                <div className='contact-item'>
+                    <h2>Contact us</h2>
+                    <div className='contact-item-content'>
+                        <p>Da Nang City</p>
+                        <p>Viet Nam</p>
+                    </div>
+                    <div className='contact-item-content'>
+                        <h5>TEL: 09 999 999</h5>
+                        <h5>MAIL: info@store.org</h5>
+                    </div>
+                    <div className='contact-item-content'>
+                        <h3>Want to join the team?</h3>
+                        <span>Apply today</span>
+                    </div>
+                </div>
+                <div className='contact-item'>
+                    <h2>Wordking hour</h2>
+                    <div className='contact-time'>
+                        <p>Mo-Fr 15:00-00:00</p>
+                        <p>Mo-Fr 11:00-00:00</p>
+                    </div>
+                    <div className='contact-item-content'>
+                        <h5>Call us at: </h5>
+                        <p>999.999.9999</p>
+                    </div>
+                    <button className='contact-btn'>
+                        <span>Order online</span>
+                    </button>
+                </div>
+                <div className='contact-item'>
+                    <h2>Get in touch</h2>
+                    <div className='contact-item-info'>
+                        <p>
+                            Bettaso family Restaurant is situated at the very
+                            historical heart of Alexandria, VA. Our address is
+                            well known to locals as the most <br /> home-like,
+                            and delicious place in the city!
+                        </p>
+                        <br />
+                        <p>
+                            Please contact us using the form on the page in
+                            regards to any questions you might have, or to make
+                            a booking with us
+                        </p>
+                    </div>
+                </div>
+                <div className='contact-form'>
+                    <form onSubmit={onHandleSubmit}>
+                        <div className='form-info'>
+                            <input
+                                className={valid.nameValid ? 'mess-error' : ''}
+                                type='text'
+                                name='name'
+                                placeholder='Your name'
+                                value={values.name}
+                                onChange={handleChange}
+                            />
+                            {valid.nameValid ? (
+                                <span>Please enter this field</span>
+                            ) : (
+                                ''
+                            )}
+                            <input
+                                className={valid.emailValid ? 'mess-error' : ''}
+                                type='email'
+                                name='email'
+                                placeholder='Email'
+                                value={values.email}
+                                onChange={handleChange}
+                            />
+                            {valid.emailValid ? (
+                                <span>Check your email again</span>
+                            ) : (
+                                ''
+                            )}
+                            <textarea
+                                className={
+                                    valid.messageValid
+                                        ? 'input-message mess-error'
+                                        : 'input-message'
+                                }
+                                name='message'
+                                placeholder='Message'
+                                value={values.message}
+                                onChange={handleChange}
+                            />
+                            {valid.messageValid ? (
+                                <span>Please enter this field</span>
+                            ) : (
+                                ''
+                            )}
+                        </div>
+                        <button type='submit' className='submit-btn'>
+                            <span>Submit</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
